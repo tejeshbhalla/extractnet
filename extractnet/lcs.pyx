0f5bce7e132eed6a3775b9c3281fbae572be2231
tejeshbhalla <62867013+tejeshbhalla@users.noreply.github.com>
2023-10-24 14:19:39 +0530

    Update lcs.pyx

--- extractnet/lcs.pyx
+++ extractnet/lcs.pyx
@@ -1,13 +1,5 @@
-"""
-longest common subsequence
-
-modified from the code snippets at
-http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_subsequence#Python
-
-cython -a lcs.pyx to output HTML
-"""
 import numpy as np
-
+from cython cimport boundscheck
 cimport cython
 cimport numpy as np
 from libcpp.string cimport string
@@ -17,11 +9,10 @@ from compat import bytes_list_cast
 
 cdef inline int int_max(int a, int b): return a if a >= b else b
 
-
 @cython.boundscheck(False)
 def longest_common_subsequence(X, Y):
     """Compute and return the longest common subsequence matrix
-    X, Y are list of strings"""
+    X, Y are lists of strings"""
     cdef int m = len(X)
     cdef int n = len(Y)
 
@@ -29,7 +20,7 @@ def longest_common_subsequence(X, Y):
     # lcs is bounded above by the minimum length of x, y
     assert min(m+1, n+1) < 65535
 
-    #cdef np.ndarray[np.int32_t, ndim=2] C = np.zeros([m+1, n+1], dtype=np.int32)
+    # cdef np.ndarray[np.int32_t, ndim=2] C = np.zeros([m+1, n+1], dtype=np.int32)
     cdef np.ndarray[np.uint16_t, ndim=2] C = np.zeros([m+1, n+1], dtype=np.uint16)
 
     # convert X, Y to C++ standard containers
@@ -59,7 +50,7 @@ def print_diff(X, Y):
     diff = []
     while i > 0 or j > 0:
         if i > 0 and j > 0 and X[i-1] == Y[j-1]:
-            #diff.append("  " + X[i-1])
+            # diff.append("  " + X[i-1])
             i -= 1
             j -= 1
         else:
@@ -70,10 +61,7 @@ def print_diff(X, Y):
                 diff.append("- " + X[i-1])
                 i -= 1
     diff.reverse()
-    print diff
-
-
-
+    print(diff)
 
 def check_inclusion(x, y):
     """Given x, y (formatted as input to longest_common_subsequence)
@@ -95,7 +83,7 @@ def check_inclusion(x, y):
         else:
             if j > 0 and (i == 0 or c[i][j-1] >= c[i-1][j]):
                 j -= 1
-            elif i > 0 and (j == 0 or c[i][j-1] < c[i-1][j]):
+            elif i > 0 and (j == 0 or c[i][j-1] < C[i-1][j]):
                 ret.append(False)
                 i -= 1
 
